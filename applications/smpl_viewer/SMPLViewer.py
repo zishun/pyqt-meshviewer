06825374748282f616b90ee97a03a7a34194be78
Zishun Liu <liuzishun@gmail.com>
2021-02-25 19:25:14 +0800

    unused line in shader

--- applications/smpl_viewer/SMPLViewer.py
+++ applications/smpl_viewer/SMPLViewer.py
@@ -72,7 +72,6 @@ class QGLControllerWidget(QtOpenGL.QGLWidget):
                 uniform mat4 Mvp;
                 in vec3 in_position;
                 in vec3 in_normal;
-                in vec2 in_texcoord_0;
                 out vec3 v_vert;
                 out vec3 v_norm;
                 void main() {
